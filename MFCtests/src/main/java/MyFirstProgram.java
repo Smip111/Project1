672c942709bbf29b6d9730a19485daeab148ff78
Smip111 <smip@inbox.ru>
2017-07-02 17:33:27 +0300

    Test program added

---
+++ MFCtests/src/main/java/MyFirstProgram.java
@@ -0,0 +1,13 @@
+﻿public class MyFirstProgram {
+	public static void main(String[] args) {
+		hello("World");
+        hello("user");
+
+    }
+
+	public static void hello(String somebody){
+        System.out.println("Hello, "+somebody+"!");
+    }
+
+
+}
